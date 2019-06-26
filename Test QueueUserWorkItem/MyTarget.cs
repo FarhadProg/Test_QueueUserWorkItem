b212a7d92f3619b34e72bf6fb8b1186c6fc1ddde
zzeratul zzeratul <Talfar2019@outlook.com>
2019-06-26 15:54:42 +0300

    Добавьте файлы проекта.

---
+++ Test QueueUserWorkItem/MyTarget.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Concurrent;
+using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Test_QueueUserWorkItem
+{
+    [Target("MyFirst")]
+    public sealed class MyTarget : TargetWithContext
+    {
+        private ManualResetEvent _doneEvent;
+        private delegate void SelectWriteMethod(LogEventInfo logEvent);
+        private SelectWriteMethod selector;
+       
+        public ConcurrentDictionary<string, string> FilePathPatternCollection { get; set; }
+
+        [RequiredParameter]
+        public string FilePathPattern { get; set; }
+        [RequiredParameter]
+        public string LogDirectory { get; set; }
+
+    
+
+
+        public MyTarget()
+        {
+            if (FilePathPattern == null)
+                FilePathPattern = @"C:\LogsTMP\Log_#.log";
+            if(LogDirectory == null)
+                LogDirectory = @"C:\LogsTMP\";
+
+            FilePathPatternCollection = new ConcurrentDictionary<string, string>();
+          
+            IncludeEventProperties = true;          
+            try
+            {
+                if (!Directory.Exists(LogDirectory))
+                {
+                    Directory.CreateDirectory(LogDirectory);
+                }
+               selector = this.WriteToLog;
+
+            }
+            catch (Exception ex)
+            {
+                selector = this.DontWriteToLog;
+                Console.WriteLine("В конструктре класса произошла ошибка " + ex.Message);
+            }
+
+
+
+        }
+
+
+        public static void ThreadPoolCallback(Object threadContext)
+        {
+            int threadIndex = (int)threadContext;
+            
+            Console.WriteLine($"Thread {threadIndex} result calculated...");
+           
+        }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+
+            selector(logEvent);
+        }
+
+        private void WriteToLog(LogEventInfo logEvent)
+        {
+            try
+            {
+
+                IDictionary<string, object> logProperties = GetAllProperties(logEvent);
+                string threadId = logProperties["@threadId"].ToString();
+                Console.WriteLine("Таргет в потоке {0} начал работу...", threadId);
+                string fullPathToFile = GetFileName(threadId);
+
+
+                string logMessage = RenderLogEvent(Layout, logEvent);
+                
+                using (StreamWriter sw = new StreamWriter(fullPathToFile, true))
+                {
+                    sw.WriteLine(logMessage);
+
+                }
+                Console.WriteLine("Таргет в потоке {0} закончил работу...",  threadId);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("При записи сообщения в лог произошла ошибка: {0}", ex.Message);
+            }
+            finally
+            {
+              
+                _doneEvent.Set();
+            }
+        }
+        private void DontWriteToLog(LogEventInfo logEvent)
+        {
+
+        }
+        private string  GetFileName(string threadID)
+        {
+            return(FilePathPatternCollection.GetOrAdd(threadID, FilePathPattern.Replace("#", threadID)));
+             
+        }
+
+
+    }
+}
---
+++ Test QueueUserWorkItem/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NLog;
+
+namespace Test_QueueUserWorkItem
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int numberOfThreads = 20;
+                var doneEvents = new ManualResetEvent[numberOfThreads];
+                for (int i = 0; i < numberOfThreads; i++)
+                {
+                    doneEvents[i] = new ManualResetEvent(false);
+                    ThreadPool.QueueUserWorkItem(JobForAThread);
+                }
+                    
+                WaitHandle.WaitAll(doneEvents);
+                //Console.WriteLine("All calculations are complete.");
+                Console.WriteLine("Логирование {0} потоков завершено", numberOfThreads);
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+
+        static void JobForAThread(object state)
+        {
+            try
+            {
+                
+                GenerateLog();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("В потоке " + Thread.CurrentThread.ManagedThreadId.ToString() +
+                    " произошла ошибка:" + ex.Message);
+            }
+
+        }
+
+        static void GenerateLog()
+        {
+            try
+            {
+
+                Logger log = LogManager.GetCurrentClassLogger();
+                log.Trace("trace message in thread {0}", Thread.CurrentThread.ManagedThreadId);
+                log.Debug("debug message in thread {0}", Thread.CurrentThread.ManagedThreadId);
+                log.Info("info message in thread {0}", Thread.CurrentThread.ManagedThreadId);
+                log.Warn("warn message in thread {0}", Thread.CurrentThread.ManagedThreadId);
+                log.Error("error message in thread {0}", Thread.CurrentThread.ManagedThreadId);
+                log.Fatal("fatal message in thread {0}", Thread.CurrentThread.ManagedThreadId);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+
+            }
+        }
+    }
+}
